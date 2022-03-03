f0844d437664b5ef8bde46530c3604c291771ed3
findthefunction <findthefunction@gmail.com>
2022-03-03 07:30:30 -0800

    added input functionality

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -8,11 +8,27 @@ import { Loader } from './';
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input 
+        placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+);
+
 const Welcome = () => {
 
     const connectWallet = () => {
 
     }
+
+    const handleSubmit = () =>{
+
+    }
+    
     return (
         <div className='flex w-full justify-center items-center'>
             <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
@@ -69,12 +85,29 @@ const Welcome = () => {
                                         <p className='text-white font-semibold text-lg mt-1'>
                                             Ethereum
                                         </p>
-                                    
-                                
                             </div>
                         </div>
                     </div>
+                        <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                            <input placeholder="Address To" name="addressTo" type="text" handleChange={()=> {}} />
+                            <input placeholder="Amount (ETH)" name="amount" type="text" handleChange={()=> {}} />
+                            <input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={()=> {}} />
+                            <input placeholder="Enter Message" name="message" type="text" handleChange={()=> {}} />
 
+                            <div className="h-[1px] w-full bg-gray-400 my-2"/>
+
+                            {false ? (
+                                <Loader />
+                            ) : (
+                                <button
+                                type="button"
+                                onClick={handleSubmit}
+                                >
+
+                                </button>
+                            )}
+
+                        </div>
                 </div>
             </div>
         </div>
